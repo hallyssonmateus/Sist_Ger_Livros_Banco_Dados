48d55feed574f9e72cf1d20265e7ab7230be5781
hallyssonmateus <hallysson_gato@hotmail.com>
2024-09-06 15:08:35 -0300

    feat: Adicionando Func. Requeridas Tabela livros

--- script.sql
+++ script.sql
@@ -26,3 +26,27 @@ CREATE TABLE Loans (
 	FOREIGN KEY (book_id) REFERENCES Books(book_id),
 	FOREIGN KEY (user_id) REFERENCES Users(user_id)
 )
+
+-- Livros
+-- Adicionando novos livros
+INSERT INTO Books (title, author, genre, published_year) 
+VALUES 
+('The Great Gatsby', 'F. Scott Fitzgerald', 'Fiction', 1925),
+('To Kill a Mockingbird', 'Harper Lee', 'Fiction', 1960),
+('1984', 'George Orwell', 'Dystopian', 1949),
+('The Catcher in the Rye', 'J.D. Salinger', 'Fiction', 1951),
+('Pride and Prejudice', 'Jane Austen', 'Romance', 1813);
+-- Atualizando informações de livros existentes.
+UPDATE Books SET author = 'Tiririca', genre = 'Comedy'
+Where book_id = 1;
+
+UPDATE Books SET title = 'Journey to the Centre of the Earth', author = 'Júlio Verne', genre = 'Adventure', published_year='2008'
+Where book_id = 4;
+
+-- Excluir livros do catálogo.
+DELETE FROM Books Where book_id=5;
+
+-- Buscar livros no catálogo portítulo, autor, gênero, etc.
+SELECT * FROM Books
+--Where title = '1984'
+Where author = 'Tiririca'
